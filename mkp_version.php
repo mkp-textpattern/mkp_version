205dfcb9f2c6221e53d4d2c2311ac440acdf5b8a
Michael K Pate <michaelkpate@gmail.com>
2015-07-09 17:56:34 -0400

    Update mkp_version.php

--- mkp_version.php
+++ mkp_version.php
@@ -5,3 +5,22 @@
 
 		return $version;
 	}
+	
+	function mkp_if_version($atts)
+	{
+		extract(lAtts(array(
+			'version'   => '',
+		),$atts));
+
+		$currentversion = $version;
+		
+		extract(get_prefs());
+
+		if (strstr($version, $currentversion)) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+
+	}
+
